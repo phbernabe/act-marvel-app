2831c2a7578f7d9403b1ec06bda5695a6fef58e6
Pedro Bernabé <pedro.bernabe@cubeti.com.br>
2022-01-10 16:41:55 -0300

    new publish

--- app/shared/marvel/marvel-config.js
+++ app/shared/marvel/marvel-config.js
@@ -1,9 +1,7 @@
 ﻿angular.module('marvel-app').value('config', {
     host: 'https://gateway.marvel.com',
-    // pubkey: '5a237863b3cc2061003cbbc4fe20dc06',
-    // privkey: 'fbf255068eccea6d0ef951b9f25626b57ab2fe72',
-    pubkey: 'deb0aa0a1b27a31df5dae071e705560d',
-    privkey: 'e78e90e130b1bf78d684baf1fa6b1cd33ec31c33',
+    pubkey: '5a237863b3cc2061003cbbc4fe20dc06',
+    privkey: 'fbf255068eccea6d0ef951b9f25626b57ab2fe72',
     apis: {
         search: '/v1/public/characters',
         details: '/v1/public/characters/{characterId}'
